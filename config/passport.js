e3ed9baa3748c51a6c1c2d2299d2b28f3409fa28
sandeepgit32 <sandeep7632@gmail.com>
2020-03-07 07:54:46 +0530

    script modified

--- config/passport.js
+++ config/passport.js
@@ -18,25 +18,23 @@ module.exports = function (passport) {
 			passReqToCallback: true // allows us to pass back the entire request to the callback
 		},
 		function (req, email, password, done) {
-			process.nextTick(function () {
-				User.findOne({
-					'local.email': email
-				}, function (err, user) {
-					if (err)
-						return done(err);
-					if (user) {
-						return done(null, false, req.flash('signupFailureMessage', 'That email is already taken.'));
-					} else {
-						var newUser = new User();
-						newUser.local.email = email;
-						newUser.local.password = newUser.generateHash(password);
-						newUser.save(function (err) {
-							if (err)
-								throw err;
-							return done(null, newUser);
-						});
-					}
-				});
+			User.findOne({
+				'local.email': email
+			}, function (err, user) {
+				if (err)
+					return done(err);
+				if (user) {
+					return done(null, false, req.flash('signupFailureMessage', 'That email is already taken.'));
+				} else {
+					var newUser = new User();
+					newUser.local.email = email;
+					newUser.local.password = newUser.generateHash(password);
+					newUser.save(function (err) {
+						if (err)
+							throw err;
+						return done(null, newUser);
+					});
+				}
 			});
 		}));
 
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 
-var morgan       = require('morgan');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var configDB = require('./config/database.js');
 
